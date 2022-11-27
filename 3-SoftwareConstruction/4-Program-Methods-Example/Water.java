02a1dbf80bde992ebc4b069fe40e3eed64659734
chobotaraleks <79744088+chobotaraleks@users.noreply.github.com>
2022-11-27 02:35:41 +0200

    Update Water.java

--- 3-SoftwareConstruction/4-Program-Methods-Example/Water.java
+++ 3-SoftwareConstruction/4-Program-Methods-Example/Water.java
@@ -1,1 +1,28 @@
+package com.company.water;
 
+public class Water {
+
+    private int water_quality; 
+    private String data; 
+    private User human; 
+    private int amount_water; 
+
+
+    public User getHuman() {
+        return human;
+    }
+
+    public int recordWater(int water_quality, int amount_water, String data){
+        this.amount_water = amount_water; 
+        this.water_quality = water_quality; 
+        this.data = data; 
+        return 0;
+    }
+
+    public int[] viewWater(){ 
+        int A[] = new int[2]; 
+        A[0] = water_quality; 
+        A[1] = amount_water; 
+        return A; 
+    }
+}
