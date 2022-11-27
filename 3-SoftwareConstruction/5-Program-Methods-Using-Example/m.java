5fbff2274036dfb78659a8871710b3beb850f5f8
chobotaraleks <79744088+chobotaraleks@users.noreply.github.com>
2022-11-27 02:51:50 +0200

    Update m.java

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example/m.java
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example/m.java
@@ -1,1 +1,15 @@
+package com.company;
 
+import com.company.water.Water;
+import com.company.water.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User("Aleksandra", "fkfkwaf1324", "chob@gmail.com"); 
+        Water water = new Water(); 
+        water.recordWater(2, 1, "27.11.2003"); 
+        user.setWater(water); 
+        System.out.println(user); 
+    }
+}
